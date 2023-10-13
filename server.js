9ae234ebbd57b1e9f990b1b91ffd6ae534e18d14
Mr. Brown <bro22002@byui.edu>
2023-10-13 22:33:44 +0000

    CSE340 Project

--- server.js
+++ server.js
@@ -6,14 +6,33 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", baseController.buildHome) /*{
+  res.render("index", {title: "Home"})
+})*/
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
